66652e4e7a82c8d7905590b6f0d8ad8b98d51401
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-05-15 19:17:19 -0300

    CRM-2 feat: Create login page
    Description:
    Implemented a new login page to allow users to authenticate and access the application. The login page includes fields for email and password, and provides necessary validations and error handling to ensure a smooth user experience.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Livewire\Auth\Login;
+use App\Livewire\Auth\Register;
 use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', Welcome::class)->name('dashboard');
-Route::get('/register', \App\Livewire\Auth\Register::class)->name('auth.register');
+Route::get('/login', Login::class)->name('login');
+Route::get('/register', Register::class)->name('register');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', Welcome::class)->name('dashboard');
+});
