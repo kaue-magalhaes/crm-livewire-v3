3a9175e67828063aa3adb3ff7cc1914458350df5
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-06-17 15:25:52 -0300

    CRM-4 feat: Implement backend functionality for password reset token validation
    Description:
    Developed the backend functionality to handle password reset token validation, ensuring that users are properly redirected based on the validity of their token.
    
    - Implemented logic to validate the password reset token sent to the user's email.
    - Users are redirected to the password reset page if the token is valid.
    - Users are redirected to the login page if the token is invalid.
    
    Note: The frontend for the password reset page and the action to actually change the password are still pending development.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Livewire\Auth\Login;
 use App\Livewire\Auth\PasswordRecovery;
+use App\Livewire\Auth\PasswordReset;
 use App\Livewire\Auth\Register;
 use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Route;
@@ -9,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/login', Login::class)->name('login');
 Route::get('/register', Register::class)->name('register');
 Route::get('/password-recovery', PasswordRecovery::class)->name('password.recovery');
-Route::get('/password-reset', fn () => 'oi')->name('password.reset');
+Route::get('/password-reset', PasswordReset::class)->name('password.reset');
 
 Route::middleware('auth')->group(function () {
     Route::get('/', Welcome::class)->name('dashboard');
