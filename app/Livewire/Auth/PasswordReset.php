0618783e1b323a7f6ebb302e8d21a11c7e764e5f
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-06-18 16:52:10 -0300

    CRM-4 feat: Implement password update functionality
    Description:
    Developed the functionality to update user passwords, ensuring secure and correct password changes.
    
    - Implemented the backend logic for updating user passwords.
    - Ensured that the password is properly hashed before saving to the database.
    - Added validation to ensure strong passwords and correct data input.
    
    Testing:
    - Created tests to verify that the password is updated correctly and securely.
    - Ensured that the tests check for the successful change of password and proper handling of the password update process.

--- app/Livewire/Auth/PasswordReset.php
+++ app/Livewire/Auth/PasswordReset.php
@@ -2,18 +2,30 @@
 
 namespace App\Livewire\Auth;
 
+use Illuminate\Auth\Events\PasswordReset as PasswordResetEvent;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class PasswordReset extends Component
 {
     public ?string $token = null;
 
-    public function mount(): void
+    public ?string $email = null;
+
+    public ?string $email_confirmation = null;
+
+    public ?string $password = null;
+
+    public ?string $password_confirmation = null;
+
+    public function mount(?string $token = null, ?string $email = null): void
     {
-        $this->token = request('token');
+        $this->token = request('token', $token);
+        $this->email = request('email', $email);
 
         if ($this->tokenNotValid()) {
 
@@ -26,6 +38,24 @@ class PasswordReset extends Component
     public function render(): View
     {
         return view('livewire.auth.password-reset');
+    }
+
+    public function updatePassword()
+    {
+        $status = Password::reset(
+            $this->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) {
+                $user->password       = $password;
+                $user->remember_token = Str::random(60);
+                $user->save();
+
+                event(new PasswordResetEvent($user));
+            }
+        );
+
+        session()->flash('status', __($status));
+
+        $this->redirectRoute('dashboard');
     }
 
     private function tokenNotValid(): bool
--- tests/Feature/Auth/Password/ResetTest.php
+++ tests/Feature/Auth/Password/ResetTest.php
@@ -1,12 +1,15 @@
 <?php
 
 use App\Livewire\Auth\PasswordRecovery;
+use App\Livewire\Auth\PasswordReset;
 use App\Models\User;
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Notification;
 use Livewire\Livewire;
 
 use function Pest\Laravel\get;
+use function PHPUnit\Framework\assertTrue;
 
 test('need to receive a valid token with a combination with the email', function () {
     Notification::fake();
@@ -25,6 +28,40 @@ test('need to receive a valid token with a combination with the email', function
 
             get(route('password.reset') . '?token=any-token')
                 ->assertRedirect(route('login'));
+
+            return true;
+        }
+    );
+});
+
+test('test if is possible to reset the password with the given token', function () {
+    Notification::fake();
+
+    $user = User::factory()->create();
+    Livewire::test(PasswordRecovery::class)
+        ->set('email', $user->email)
+        ->call('requestPasswordRecovery');
+
+    Notification::assertSentTo(
+        $user,
+        ResetPassword::class,
+        function (ResetPassword $notification) use ($user) {
+            Livewire::test(
+                PasswordReset::class,
+                ['token' => $notification->token, 'email' => $user->email]
+            )
+                ->set('email_confirmation', $user->email)
+                ->set('password', 'new-password')
+                ->set('password_confirmation', 'new-password')
+                ->call('updatePassword')
+                ->assertHasNoErrors()
+                ->assertRedirect(route('dashboard'));
+
+            $user->refresh();
+
+            assertTrue(
+                Hash::check('new-password', $user->password)
+            );
 
             return true;
         }
