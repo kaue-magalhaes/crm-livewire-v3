50359d024c852d3a8b547d00d7326f2c76457dbf
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-06-20 15:43:41 -0300

    CRM-5 feat: Add UserSeeder with admin user and permission assignment
    Description:
    Enhanced the seeding process by creating a `UserSeeder` that initializes an admin user with specific permissions.
    
    Changes:
    - Created `UserSeeder` to seed the database with an admin user having the 'be an admin' permission.
    - Implemented `withPermission` method in `UserFactory` to streamline the assignment of permissions during user creation.
    - Added tests to ensure the seeder correctly seeds an admin user with the specified permissions.
    
    Testing:
    - Verified the existence of the admin user and assigned permissions in the database using seeders `PermissionSeeder` and `UserSeeder`.

--- tests/Feature/PermissionControlTest.php
+++ tests/Feature/PermissionControlTest.php
@@ -3,8 +3,10 @@
 use App\Models\Permission;
 use App\Models\User;
 use Database\Seeders\PermissionSeeder;
+use Database\Seeders\UserSeeder;
 
 use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\seed;
 
 it('should be able to give an user a permission to de something', function () {
     /** @var User $user */
@@ -27,9 +29,22 @@ it('should be able to give an user a permission to de something', function () {
 });
 
 test('permission has to have a seeder', function () {
-    $this->seed(PermissionSeeder::class);
+    seed(PermissionSeeder::class);
 
     assertDatabaseHas('permissions', [
         'key' => 'be an admin',
+    ]);
+});
+
+test('seed with an admin user', function () {
+    seed([PermissionSeeder::class, UserSeeder::class]);
+
+    assertDatabaseHas('permissions', [
+        'key' => 'be an admin',
+    ]);
+
+    assertDatabaseHas('permission_user', [
+        'user_id'       => User::query()->first()->id,
+        'permission_id' => Permission::query()->where(['key' => 'be an admin'])->first()->id,
     ]);
 });
