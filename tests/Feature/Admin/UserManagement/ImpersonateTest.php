29c164e1f39e0d85e62ead5439764e7e17e33411
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2025-02-24 16:51:51 -0300

    CRM-8 implement authorization check for user impersonation

--- tests/Feature/Admin/UserManagement/ImpersonateTest.php
+++ tests/Feature/Admin/UserManagement/ImpersonateTest.php
@@ -67,3 +67,22 @@ it('should be able to stop impersonation', function () {
 
     expect(auth()->user()->id)->toBe($admin->id);
 });
+
+it('should have the correct permission to impersonate someone', function () {
+    $admin = User::factory()->admin()->create();
+    /** @var User $nonAdmin */
+    $nonAdmin = User::factory()->create();
+    $user     = User::factory()->create();
+
+    actingAs($nonAdmin);
+
+    Livewire::test(Impersonate::class)
+        ->call('impersonate', $user->id)
+        ->assertForbidden();
+
+    actingAs($admin);
+
+    Livewire::test(Impersonate::class)
+        ->call('impersonate', $user->id)
+        ->assertRedirect();
+});
