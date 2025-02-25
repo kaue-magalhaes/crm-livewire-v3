cf966b4ce985c5465a9a1984704980e19c372022
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2025-02-25 12:18:05 -0300

    implement SendNewCode event for validation code regeneration

--- app/Livewire/Auth/EmailValidation.php
+++ app/Livewire/Auth/EmailValidation.php
@@ -2,7 +2,7 @@
 
 namespace App\Livewire\Auth;
 
-use App\Notifications\Auth\ValidationCodeNotification;
+use App\Events\SendNewCode;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Livewire\Component;
@@ -29,10 +29,8 @@ class EmailValidation extends Component
 
     public function sendNewCode(): void
     {
-        $user                  = auth()->user();
-        $user->validation_code = random_int(100000, 999999);
-        $user->save();
-
-        $user->notify(new ValidationCodeNotification);
+        SendNewCode::dispatch(
+            auth()->user()
+        );
     }
 }
